6705eb5c30bfaf90c0c92c78a9503e598699948e
宋晨阳 <2018013460@secoder.net>
2020-10-02 11:39:57 +0800

    Initial commit

--- run.sh
+++ run.sh
@@ -1,3 +1,1 @@
-python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('scy18', '', 'scy20000827')" | python manage.py shell
-python manage.py runserver 0.0.0.0:80
+npm run serve

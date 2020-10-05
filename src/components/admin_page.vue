bf147821fd86d65aafd5102676d1c747c2d745b6
张奕卓 <2018013434@secoder.net>
2020-10-05 14:38:27 +0800

    add user_page and admin_page

---
+++ src/components/admin_page.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "admin_page"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/user_page.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "user_page"
+}
+</script>
+
+<style scoped>
+
+</style>

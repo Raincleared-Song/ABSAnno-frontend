242bd3fdc6109376eb113ecd6d0de788d6f696a8
张奕卓 <2018013434@secoder.net>
2020-10-08 16:51:25 +0800

    user page update

--- src/components/user_page.vue
+++ src/components/user_page.vue
@@ -1,62 +1,83 @@
 <template>
-  <div>
+
     <el-row class="tac">
 
       <el-col :span="6">
-        <h5>自定义颜色</h5>
-        <el-menu
-            default-active="2"
-            class="el-menu-vertical-demo"
-            background-color="#545c64"
-            text-color="#fff"
-            active-text-color="#ffd04b">
-          <el-submenu index="1">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>导航一</span>
-            </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="1-4-1">选项1</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-          <el-menu-item index="2">
-            <i class="el-icon-menu"></i>
-            <span slot="title">导航二</span>
-          </el-menu-item>
-          <el-menu-item index="3" disabled>
-            <i class="el-icon-document"></i>
-            <span slot="title">导航三</span>
-          </el-menu-item>
-          <el-menu-item index="4">
-            <i class="el-icon-setting"></i>
-            <span slot="title">导航四</span>
-          </el-menu-item>
-        </el-menu>
+        <div>
+          <el-avatar :size="70" :src="circleUrl"><!--这里要从数据库拿头像，更改src--></el-avatar>
+<!--          这里要从数据库拿名字和身份，显示在下方-->
+          <h3>用户信息界面</h3>
+          <h4>用户权限：用户，发布者，管理员</h4>
+          <el-menu
+              default-active="2"
+              class="el-menu-vertical-demo"
+              background-color="#545c64"
+              text-color="#fff"
+              active-text-color="#ffd04b">
+            <el-menu-item index="1" @click="change(0)">
+              <i class="el-icon-menu"></i>
+              <span slot="title" >基本信息</span>
+            </el-menu-item>
+            <el-menu-item index="2" @click="change(1)">
+              <i class="el-icon-menu"></i>
+              <span slot="title" >答题历史</span>
+            </el-menu-item>
+            <el-menu-item index="3" @click="change(2)">
+              <i class="el-icon-document"></i>
+              <!-- 当且仅当当前用户为发布者时，可以看到这一条 -->
+              <span slot="title" >我的发布</span>
+            </el-menu-item>
+            <el-menu-item index="4" @click="change(3)">
+              <i class="el-icon-setting"></i>
+              <span slot="title">待定</span>
+            </el-menu-item>
+          </el-menu>
+        </div>
+      </el-col>
+      <el-col :span="10">
+        <div>
+<!--          填写下方的四个div中的内容，预计有表格、列表、输入框等等。-->
+          <div v-show="0===page_number">
+            <p>这是用户信息界面</p>
+          </div>
+          <div v-show="1===page_number">
+            <p>这是答题历史界面</p>
+          </div>
+          <div v-show="2===page_number">
+            <p>这是我的发布界面</p>
+          </div>
+          <div v-show="3===page_number">
+            <p>这是待定界面</p>
+          </div>
+        </div>
+
       </el-col>
     </el-row>
 
 
-  </div>
+
 </template>
 
 <script>
   export default {
-    name: "user_page"
+    name: "user_page",
+    data () {
+      return {
+        circleUrl: "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png",
+        squareUrl: "https://cube.elemecdn.com/9/c2/f0ee8a3c7c9638a54940382568c9dpng.png",
+        sizeList: ["large", "medium", "small"],
+        page_number: 0,
+      }
+    },
+    methods: {
+      change: function (index) {
+        this.page_number = index;
+      }
+    }
   }
 
-
-
 </script>
-
 <style scoped>
+
 
 </style>

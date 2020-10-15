0106b58edfd6ee0d6b6d490698108db3d372ae7e
罗境佳 <2018013469@secoder.net>
2020-10-15 09:05:45 +0800

    commitment before merging

--- src/components/create_mission.vue
+++ src/components/create_mission.vue
@@ -13,34 +13,34 @@
         </a-layout-sider>
 
         <!-- 任务预览区 -->
-        <a-layout-content style="margin: 10px 40px">
-          <h2>编辑任务</h2>
-          <div>
-            <span>任务名称：</span>
-            <a-input
-                v-model.trim="missionDescription"
-                palceholder="your mission title" />
-          </div>
-          <div>任务类型：{{ missionType }}</div>
-          <div>
-            <span>任务至少标注次数：</span>
-            <a-input-number
-                v-model.trim.number="minimumTotalAnnotation" />
-          </div>
-          <div>
-            <span>任务截止期限：</span>
-            <a-date-picker v-model="missionDdl" />
-          </div>
-          <div>
-            <span>分发对象：</span>
-            <a-select
-                v-model="selectedTags"
-                mode="multiple">
-              <a-select-option v-for="i in 25" :key="(i + 9).toString(36) + i">
-                {{ (i + 9).toString(36) + i }}
-              </a-select-option>
-            </a-select>
-          </div>
+<!--        <a-layout-content style="margin: 10px 40px">-->
+<!--          <h2>编辑任务</h2>-->
+<!--          <div>-->
+<!--            <span>任务名称：</span>-->
+<!--            <a-input-->
+<!--                v-model.trim="missionDescription"-->
+<!--                palceholder="your mission title" />-->
+<!--          </div>-->
+<!--          <div>任务类型：{{ missionType }}</div>-->
+<!--          <div>-->
+<!--            <span>任务至少标注次数：</span>-->
+<!--            <a-input-number-->
+<!--                v-model.trim.number="minimumTotalAnnotation" />-->
+<!--          </div>-->
+<!--          <div>-->
+<!--            <span>任务截止期限：</span>-->
+<!--            <a-date-picker v-model="missionDdl" />-->
+<!--          </div>-->
+<!--          <div>-->
+<!--            <span>分发对象：</span>-->
+<!--            <a-select-->
+<!--                v-model="selectedTags"-->
+<!--                mode="multiple">-->
+<!--              <a-select-option v-for="i in 25" :key="(i + 9).toString(36) + i">-->
+<!--                {{ (i + 9).toString(36) + i }}-->
+<!--              </a-select-option>-->
+<!--            </a-select>-->
+<!--          </div>-->
 
 <!--          <edit_question-->
 <!--              :username="username" :id="id"-->
@@ -48,7 +48,7 @@
 <!--              :mission_description="missionDescription"-->
 <!--              :minimum_total_annotation="minimumTotalAnnotation" />-->
 
-        </a-layout-content>
+<!--        </a-layout-content>-->
       </a-layout>
     </a-layout-content>
   </a-layout>

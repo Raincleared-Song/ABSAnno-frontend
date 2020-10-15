6df96fb90c46f64da314c049adb9ba30846b3759
罗境佳 <2018013469@secoder.net>
2020-10-15 09:26:44 +0800

    add router link to edit_question #4

--- src/components/create_mission.vue
+++ src/components/create_mission.vue
@@ -13,42 +13,51 @@
         </a-layout-sider>
 
         <!-- 任务预览区 -->
-<!--        <a-layout-content style="margin: 10px 40px">-->
-<!--          <h2>编辑任务</h2>-->
-<!--          <div>-->
-<!--            <span>任务名称：</span>-->
-<!--            <a-input-->
-<!--                v-model.trim="missionDescription"-->
-<!--                palceholder="your mission title" />-->
-<!--          </div>-->
-<!--          <div>任务类型：{{ missionType }}</div>-->
-<!--          <div>-->
-<!--            <span>任务至少标注次数：</span>-->
-<!--            <a-input-number-->
-<!--                v-model.trim.number="minimumTotalAnnotation" />-->
-<!--          </div>-->
-<!--          <div>-->
-<!--            <span>任务截止期限：</span>-->
-<!--            <a-date-picker v-model="missionDdl" />-->
-<!--          </div>-->
-<!--          <div>-->
-<!--            <span>分发对象：</span>-->
-<!--            <a-select-->
-<!--                v-model="selectedTags"-->
-<!--                mode="multiple">-->
-<!--              <a-select-option v-for="i in 25" :key="(i + 9).toString(36) + i">-->
-<!--                {{ (i + 9).toString(36) + i }}-->
-<!--              </a-select-option>-->
-<!--            </a-select>-->
-<!--          </div>-->
+        <a-layout-content style="margin: 10px 40px">
+          <h2>编辑任务</h2>
+          <div>
+            <span>任务名称：</span>
+            <a-input
+                v-model.trim="missionDescription"
+                palceholder="your mission title" />
+          </div>
+          <div>任务类型：{{ missionType }}</div>
+          <div>
+            <span>任务至少标注次数：</span>
+            <a-input-number
+                v-model.trim.number="minimumTotalAnnotation" />
+          </div>
+          <div>
+            <span>任务截止期限：</span>
+            <a-date-picker v-model="missionDdl" />
+          </div>
+          <div>
+            <span>分发对象：</span>
+            <a-select
+                v-model="selectedTags"
+                mode="multiple">
+              <a-select-option v-for="i in 25" :key="(i + 9).toString(36) + i">
+                {{ (i + 9).toString(36) + i }}
+              </a-select-option>
+            </a-select>
+          </div>
 
-<!--          <edit_question-->
-<!--              :username="username" :id="id"-->
-<!--              :mission_type="missionType"-->
-<!--              :mission_description="missionDescription"-->
-<!--              :minimum_total_annotation="minimumTotalAnnotation" />-->
+          <a-upload-dragger
+              name="file"
+              action="https://www.mocky.io/v2/5cc8019d300000980a055e76">
+            <p class="ant-upload-drag-icon">
+              <a-icon type="inbox" />
+            </p>
+            <p>
+              Click or drag file to this area to upload
+            </p>
+          </a-upload-dragger>
 
-<!--        </a-layout-content>-->
+          <a-button type="primary">
+            <router-link to="/edit">手动添加题目</router-link>
+          </a-button>
+
+        </a-layout-content>
       </a-layout>
     </a-layout-content>
   </a-layout>

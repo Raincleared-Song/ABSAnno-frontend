93da8946b8138e01bd30afcc760794bcfdcc3f87
罗境佳 <2018013469@secoder.net>
2020-10-17 21:57:26 +0800

    mission form validation ok #4

--- src/components/create_mission.vue
+++ src/components/create_mission.vue
@@ -5,50 +5,56 @@
     <a-layout-content style="margin: 10px 40px;">
       <h1>编辑任务</h1>
       <a-form-model
+          ref="mission_form"
           :model="form"
+          :rules="rules"
           :label-col="{ span: 4 }"
           :wrapper-col="{ span: 15, offset: 1 }">
         <a-form-model-item
-            ref="missionDescription"
+            ref="name"
             label="任务名称"
-            prop="name" required>
+            prop="name">
           <a-input
-              v-model.trim="form.missionDescription"
-              palceholder="your mission title"
+              v-model.trim="form.name"
+              @blur="() => { $refs.name.onFieldBlur(); }"
+              @change="() => { $refs.name.onFieldChange(); }"
               allow-clear />
         </a-form-model-item>
         <a-form-model-item
-            ref="missionType"
+            ref="type"
             label="任务类型"
-            prop="type" required>
-          <a-select v-model="form.missionType">
+            prop="type">
+          <a-select
+              v-model="form.type"
+              @blur="$refs.type.onFieldBlur()"
+              @change="$refs.type.onFieldChange()">
             <a-select-option value="judgement">判断题任务</a-select-option>
             <a-select-option value="checkbox">选择题任务</a-select-option>
             <a-select-option value="text">文字描述题任务</a-select-option>
           </a-select>
         </a-form-model-item>
         <a-form-model-item
-            ref="minimumTotalAnnotation"
-            label="标注次数下限"
-            prop="min">
+            label="标注次数下限" prop="min">
             <a-input-number
-                v-model.trim.number="form.minimumTotalAnnotation" />
+                v-model.trim.number="form.min" />
         </a-form-model-item>
         <a-form-model-item
-            ref="missionDDL"
-            label="任务截止期限"
-            prop="ddl">
-          <a-date-picker v-model="form.missionDDL" />
+            label="任务截止期限" prop="ddl">
+          <a-date-picker v-model="form.ddl" />
         </a-form-model-item>
         <a-form-model-item
-            ref="selectedTags"
-            label="分发对象"
-            prop="tags">
+            label="分发对象" prop="tags">
           <a-select
-              v-model="form.selectedTags"
+              v-model="form.tags"
               mode="multiple">
-            <a-select-option v-for="i in 25" :key="(i + 9).toString(36) + i">
-              {{ (i + 9).toString(36) + i }}
+            <a-select-option :key="1">
+              学生
+            </a-select-option>
+            <a-select-option :key="2">
+              教师
+            </a-select-option>
+            <a-select-option :key="3">
+              程序员
             </a-select-option>
           </a-select>
         </a-form-model-item>
@@ -56,10 +62,8 @@
 
       <div>
         请上传文件或者
-        <a-button type="link">
-          <router-link to="/edit/judgement">
+        <a-button type="link" @click="onEditClick">
             手动添加题目
-          </router-link>
         </a-button>
       </div>
       <div style="margin: 20px;">
@@ -85,12 +89,30 @@ export default {
   data() {
     return {
       form: {
-        missionType: "",
-        missionDescription: "",
-        minimumTotalAnnotation: 10,
-        missionDDL: null,
-        selectedTags: []
+        name: '',
+        type: '',
+        min: 10,
+        ddl: undefined,
+        tags: []
+      },
+      rules: {
+        name: [{ required: true, message: 'Mission name cannot be null.', trigger: 'blur' }],
+        type: [{ required: true, message: 'Mission type cannot be null.', trigger: 'blur' }],
+        min: [{ required: false }],
+        ddl: [{ required: false }],
+        tags: [{ required: false }]
       }
+    };
+  },
+  methods: {
+    onEditClick() {
+      this.$refs.mission_form.validate(valid => {
+        if (valid) {
+          this.$router.push(`/edit/${this.form.type}`);
+        } else {
+          this.$message.warning("error submit");
+        }
+      });
     }
   }
 }

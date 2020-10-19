aa8584dcea10d4c0309d7b60d1ec97f53a7a408b
谢昱清 <2018013428@secoder.net>
2020-10-19 09:55:42 +0800

    add crsf token to login&signin pages

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueCookies from 'vue-cookies'
+
 import router from './router/router.js'
 import Antd from 'ant-design-vue'// 引入Ant Design Vue组件
 import 'ant-design-vue/dist/antd.css' // 引入Ant Design Vue样式
@@ -9,6 +11,7 @@ import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 Vue.use(Antd) //挂载到vue中
 Vue.config.productionTip = false
+Vue.use(VueCookies)
 
 new Vue({
   render: h => h(App),

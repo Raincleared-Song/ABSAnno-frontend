654c50d8da6969c1ebad6473fea3bd3007d9ea36
罗境佳 <2018013469@secoder.net>
2020-11-01 11:02:54 +0800

    jest configuration

---
+++ src/__tests__/app.test.js
@@ -0,0 +1,17 @@
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import ElementUI from "element-ui";
+import Antd from "ant-design-vue";
+import App from "@/App";
+import VueRouter from "vue-router";
+
+let localVue = createLocalVue();
+localVue.use(ElementUI);
+localVue.use(Antd);
+localVue.use(VueRouter);
+
+describe('App', () => {
+    test('App default power', () => {
+        const wrapper = shallowMount(App, { localVue });
+        expect(wrapper.vm.power).toBe(-1);
+    })
+})

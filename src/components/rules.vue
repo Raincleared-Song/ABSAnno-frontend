855280a3c7b61d50a9a444ee276d3f67c67939ad
谢昱清 <2018013428@secoder.net>
2020-11-17 21:13:17 +0800

    reload rules page #23

--- src/components/rules.vue
+++ src/components/rules.vue
@@ -1,14 +1,44 @@
 <template>
-    <div>
-        <p>规则说明</p>
+    <div style="padding:20px 80px 20px 80px;">
+        <h2 style="text-align: center;">规则说明</h2>
+
+        <a-tree>
+            <!--            <a-icon slot="switcherIcon" type="down" />-->
+            <a-tree-node key="0-0" title="对于做题者" style="font-size: 16px">
+                <a-tree-node key="0-0-0" title="你的义务" style="font-size: 14px">
+                    <a-tree-node key="0-0-0-0" title="你应该仔细阅读每个题的介绍、问题和选项，并且给出你认可的答案。" />
+                    <a-tree-node key="0-0-0-1" title="你不应该对本网站以及对应后端进行任何形式、任何程度的攻击。" />
+                    <a-tree-node key="0-0-0-2" title="如果我们的反作弊机制发现你有过多的作弊行为或者不认真答题的行为，我们有权对你的账号进行封禁。" />
+                </a-tree-node>
+                <a-tree-node key="0-0-1" title="你的权利" style="font-size: 14px">
+                    <a-tree-node key="0-0-1-0" title="你可以通过积累金币或者充值金币来申请成为发题者。" />
+                    <a-tree-node key="0-0-1-1" title="你可以在广场上自由答题；如果答案符合我们的先验答案，你会获得奖励，并且在之后的答题过程中有更高的可信度。" />
+                    <a-tree-node key="0-0-1-2" title="你可以正常使用所有普通用户的功能，包括但不限于：题目搜索，兴趣列表，查看答题历史，修改个人信息及密码。" />
+                </a-tree-node>
+            </a-tree-node>
+            <a-tree-node key="0-1" title="对于发题者" style="font-size: 16px">
+                <a-tree-node key="0-1-0" title="你的义务" style="font-size: 14px">
+                    <a-tree-node key="0-1-0-0" title="你应该尽可能完整地填写发布的题目各种内容，包括但不限于：题目、介绍、先验答案、题目标签。" />
+                    <a-tree-node key="0-1-0-1" title="你不应该对本网站以及对应后端进行任何形式、任何程度的攻击。" />
+                    <a-tree-node key="0-1-0-2" title="你不能在任何程度上违反中国的相关法律法规。如果有，我们有权对你的账号进行封禁。" />
+                </a-tree-node>
+                <a-tree-node key="0-1-1" title="你的权利" style="font-size: 14px">
+                    <a-tree-node key="0-1-1-0" title="你可以在相应页面发布符合规定的题目，并且给那些正确答题的用户支付报酬。" />
+                    <a-tree-node key="0-1-1-1" title="你可以随时查看题目的结果，并且下载对应的结果文件。" />
+                    <a-tree-node key="0-1-1-2" title="你可以选择忽略某些用户的作答。" />
+                </a-tree-node>
+            </a-tree-node>
+        </a-tree>
     </div>
+
 </template>
 
 <script>
     export default {
-        name: "rules"
+        name: "rules",
     }
 </script>
+
 
 <style scoped>
 
